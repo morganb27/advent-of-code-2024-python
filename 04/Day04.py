ca90b7caefd9b5790cac90043c7de482269e989a
morganb27 <morgan.ballouk@gmail.com>
2024-12-07 11:24:42 +0100

    Day 4 part 2

--- 04/Day04.py
+++ 04/Day04.py
@@ -3,7 +3,8 @@ from utils import Point, DIRS_8
 
 PUZZLE = [list(line.strip()) for line in fileinput.input()]
 BOARD = {}
-PART_1 = 0
+PART_1, PART_2 = 0, 0
+DIRS_4 = [Point(1, 1), Point(-1, 1), Point(-1, -1), Point(1, -1)]
 
 # Parse the input
 for i in range(len(PUZZLE)):
@@ -23,6 +24,16 @@ for point in BOARD:
             ):
                 PART_1 += 1
 
+# Part 2
+for point in BOARD:
+     if BOARD.get(point) == 'A':
+          diag_one = set([BOARD.get(point + Point(1, 1)), BOARD.get(point + Point(-1, -1))])
+          diag_two = set([BOARD.get(point + Point(1, -1)), BOARD.get(point + Point(-1, 1))])
+          if diag_one == diag_two == set(['M', 'S']):
+            PART_2 += 1
+    
+    
 print(f"Solution to part 1 is: {PART_1}")
+print(f"Solution to part 1 is: {PART_2}")
 
     
