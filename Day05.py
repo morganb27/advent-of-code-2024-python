942a8b5c4afa7496648afafec687523aa82d07ab
morganb27 <morgan.ballouk@gmail.com>
2024-12-08 19:05:09 +0100

    Day 5 part 2

--- Day05.py
+++ Day05.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 PUZZLE = "".join(line for line in fileinput.input())
 RULES, UPDATES = map(str.split, PUZZLE.split("\n\n"))
-PART_1 = 0
+PART_1, PART_2 = 0, 0
 
 # Parse the input
 RULES_DICT = defaultdict(list)
@@ -12,20 +12,33 @@ for rule in RULES:
     RULES_DICT[left].append(right)
 
 UPDATES = list(line.split(',') for line in UPDATES)
+UNORDERED = []
 
 # Solve problem
 
 # Part 1
-VALID = []
 for update in UPDATES:
     for i, page in enumerate(update):
         if not all(next_page in RULES_DICT[page] for next_page in update[i+1:]):
+            UNORDERED.append(update)
             break
     else:
         middle = len(update)//2
         PART_1 += int(update[middle])
+
+
+# Part 2
+for unordered_update in UNORDERED:
+    n = len(unordered_update)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if unordered_update[j] in RULES_DICT[unordered_update[j + 1]]:
+                unordered_update[j], unordered_update[j + 1] = unordered_update[j + 1], unordered_update[j]
+    middle = len(unordered_update)//2
+    PART_2 += int(unordered_update[middle])
     
 print(f"Solution to part 1 is: {PART_1}")
+print(f"Solution to part 2 is: {PART_2}")
     
                 
 
