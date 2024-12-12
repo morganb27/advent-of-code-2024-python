a4c38a0c87ce02c0d064d4a0d4ae2497177cb9b5
morganb27 <morgan.ballouk@gmail.com>
2024-12-12 09:18:30 +0100

    Day 8 part 1

--- Day07.py
+++ Day07.py
@@ -3,25 +3,33 @@ import itertools
 
 PUZZLE = [line.strip() for line in fileinput.input()]
 
-PART_1 = 0
-
 # Solve problem
 # Part 1
-for line in PUZZLE:
-    ops = ["+", "*"]
-    left, right = line.split(": ")
-    left = int(left)
-    nums = list(map(int, right.split()))
-    for operators in itertools.product(ops, repeat=(len(nums) - 1)):
-        result = int(nums[0])
-        for a, b in zip(nums[1:], operators):
-            if b == "+":
-                result += a
-            elif b == "*":
-                result *= a
-        if result == left:
-            PART_1 += result
-            break
+def solve(data, part_2=False):
+    sum = 0
+    for line in data:
+        ops = ["+", "*"]
+        if part_2:
+            ops.append("||")
+        left, right = line.split(": ")
+        left = int(left)
+        nums = list(map(int, right.split()))
+        for operators in itertools.product(ops, repeat=(len(nums) - 1)):
+            result = int(nums[0])
+            for a, b in zip(nums[1:], operators):
+                if b == "+":
+                    result += a
+                elif b == "*":
+                    result *= a
+                elif b == "||":
+                    result = int('{}{}'.format(result, a))
+            if result == left:
+                sum += result
+                break
+    return sum
 
+PART_1 = solve(PUZZLE)
+PART_2 = solve(PUZZLE, True)
             
 print(f"Solution to part 1 is: {PART_1}")
+print(f"Solution to part 2 is: {PART_2}")
---
+++ Day08.py
@@ -0,0 +1,27 @@
+import fileinput
+from collections import defaultdict
+from utils import Point
+import itertools
+
+PUZZLE = [list(line.strip()) for line in fileinput.input()]
+MAP = defaultdict(list)
+ANTINODES = set()
+
+# Parse the input
+for y in range(len(PUZZLE)):
+    for x in range(len(PUZZLE[0])):
+        if PUZZLE[y][x] != ".":
+            MAP[PUZZLE[y][x]].append(Point(x, y))
+
+
+# Solve Part 1
+for key, values in MAP.items():
+    for a, b in itertools.permutations(values, 2):
+            anti_node = b + (b - a)
+            if 0 <= anti_node.x < len(PUZZLE[0]) and 0 <= anti_node.y < len(PUZZLE):
+                ANTINODES.add(anti_node)
+
+PART_1 = len(ANTINODES)
+
+print(f"Solution to part 1 is: {PART_1}")
+            
