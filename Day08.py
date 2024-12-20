e4ff409efec07f1bd62c509a754acbb00765ba07
morganb27 <morgan.ballouk@gmail.com>
2024-12-20 10:48:35 +0100

    Day 8 part 2

--- Day08.py
+++ Day08.py
@@ -17,11 +17,13 @@ for y in range(len(PUZZLE)):
 # Solve Part 1
 for key, values in MAP.items():
     for a, b in itertools.permutations(values, 2):
+            ANTINODES.add(b)
             anti_node = b + (b - a)
-            if 0 <= anti_node.x < len(PUZZLE[0]) and 0 <= anti_node.y < len(PUZZLE):
+            while 0 <= anti_node.x < len(PUZZLE[0]) and 0 <= anti_node.y < len(PUZZLE):
                 ANTINODES.add(anti_node)
+                anti_node += (b - a)
 
-PART_1 = len(ANTINODES)
+PART_2 = len(ANTINODES)
 
-print(f"Solution to part 1 is: {PART_1}")
+print(f"Solution to part 1 is: {PART_2}")
             
